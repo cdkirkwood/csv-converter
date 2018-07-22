b30e332f8f5c52540f36a0f1bb808efdebf8a402
Connor Kirkwood <cdkirkwood@gmail.com>
2018-07-22 01:29:04 -0400

    columns are sortable

--- client/src/App.js
+++ client/src/App.js
@@ -11,31 +11,39 @@ class App extends Component {
     }
 
     this.sortColumn = this.sortColumn.bind(this)
-    this.sortStringData = this.sortStringData.bind(this)
-    this.sortIntData = this.sortIntData.bind(this)
+    this.sortData = this.sortData.bind(this)
+    //this.sortIntData = this.sortIntData.bind(this)
   }
 
   componentDidMount() {
     //fetching csv json from server
     fetch('/api/csv')
       .then(res => res.json())
-      .then(data => this.setState({ csvTitles: data[0], csvData: data.slice(1) }))
+      .then(data => this.setState({ csvTitles: data[0], csvData: data.slice(1, data.length - 1) }))
   }
 
   sortColumn(evt, index) {
     const data = this.state.csvData
+    const title = this.state.csvTitles[index]
     const isString = isNaN(data[0][index])
-    const sortedData = isString ? this.sortStringData(data) : this.sortIntData(data)
+    // const sortedData = isString ? this.sortData(data, index) : this.sortIntData(data, index)
+    const sortedData = this.sortData(data, index)
+    //const sortedKey = 'is' + title + 'Sorted'
+    //need to somehow keep track of if the column is already sorted
     this.setState({csvData: sortedData})
   }
 
-  sortStringData(data) {
-    return data.sort()
+  sortData(data, index) {
+    return data.sort((a, b) => {
+      if (a[index] > b[index]) return 1
+      if (a[index] < b[index]) return -1
+      else return 0
+    })
   }
 
-  sortIntData(data) {
-    return data.sort((a, b) => a - b)
-  }
+  // sortIntData(data) {
+  //   return data.sort((a, b) => a - b)
+  // }
 
   render() {
     const titles = this.state.csvTitles
